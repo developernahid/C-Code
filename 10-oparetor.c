35252910ad979dfe473f06addf1a62122e97b0cd
developernahide <topperdigitalmarketer@gmail.comm>
2021-09-17 22:42:30 +0600

    Nahid add

---
+++ 10-oparetor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){ 
+    int a = 5;
+    a+= 50; 
+    // a+/a-  dile jog/biyog hobbe R a++; mane 1 jog
+
+    printf("The Vlue of oparetor: %d", a);
+
+
+
+
+    return 0;
+}
